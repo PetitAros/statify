14f3ded33206a87e6e1e3515fad648685e00b3d8
Erwan Lecomte <127357702+PetitAros@users.noreply.github.com>
2026-05-26 18:27:07 +0000

    fix: env variable

--- src/service/spotifyAuth.js
+++ src/service/spotifyAuth.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+const REDIRECT_URI = window.location.origin + '/';
 const SCOPES = ["user-read-currently-playing", "user-read-recently-played", "user-top-read"];
 
 export async function redirectToAuthCodeFlow() {
